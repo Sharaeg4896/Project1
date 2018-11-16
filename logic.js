e88935a4756c3a20ab029a10aa0160e36e192f64
SgtStarshine <starshine621@ymail.com>
2018-11-15 21:45:46 -0500

    deleted commented and unusable code in logic script, and small edits on index-ph page

--- logic.js
+++ logic.js
@@ -20,21 +20,6 @@ $(document).ready( function () {
     // Event listener for translating user input data into the Trip Planner Entries (TPE) card
     $("#entrySubmit").on("click", function(event) {
         event.preventDefault();
-        
-        // var dateFormat = "MM/DD/YYYY";
-
-        // Entries in the form are stored in variables
-        // var dest = $("#destInput").val().trim();
-        // var sDate = $("#start-date").val().trim();
-        // var eDate = $("#end-date").val().trim();
-
-        // Reconverts date & time format using momentJS
-        // var startD = moment(sDate, dateFormat).format("MMM Do YYYY");
-        // var endD = moment(eDate, dateFormat).format("MMM Do YYYY");
-
-        // Creating new entries for the TPE card
-        // var newEntry = $("<tr>");
-        // $("#newEntryList").append(newEntry);
 
         entryRef.push({
             destination: $('#destInput').val().replace(/<[^>]*>/ig, ""),
@@ -44,20 +29,6 @@ $(document).ready( function () {
         });
 
         entryRef.reset();
-
-        // Summited info is converted to be displayed in TPE card
-        // var destEntryItem = $("<td>").text(dest);
-        // var sDateEntryItem = $("<td>").text(startD);
-        // var eDateEntryItem = $("<td>").text(endD);
-        
-        // destEntryItem.attr("scope", "col");
-        // DateEntryItem.attr("scope", "col");
-        // eDateEntryItem.attr("scope", "col");
-
-        // Submitted info appended to the new entry for the TPE card
-        // newEntry.append(destEntryItem);
-        // newEntry.append(sDateEntryItem);
-        // newEntry.append(eDateEntryItem);
 
         $("#destInput").val('');
         $("#start-date").val('');
@@ -81,51 +52,6 @@ entryRef.on("child_added", function(snap) {
     console.log("added", snap.key, snap.val());
     $('#newEntryList').append(contactHtmlFromObject(snap.val()));
   });
-
-
-$("#load-entry").on("click", function(event) {
-    event.preventDefault();
-
-    // Pulls the existing itinerary ID out of the text input and assigns it to a variable.
-    id  = $("#ID-Input").val().trim();
-    console.log(id);
-    // Do a one-time read from Firebase, reading the destination objects that were created/written to Firebase on the "itinerary builder" page.  
-    database.ref('').once('value').then(function(snapshot){
-        console.log(snapshot.val());
-        itinLoad = snapshot.val();
-        console.log('---------');
-
-        // Since each individual destination returns as an individual object separately from one another, we need to convert that list of objects to an array in order to loop through it and pull the data from each one, in order to propagate our table.
-        var itinArray = Object.values(itinLoad)
-        console.log("Number of objects in itinerary array: " + itinArray.length);
-        console.log(itinArray);
-
-        var itinList = $("<ul></ul>");
-
-        for (var i = 0; i < itinArray.length; i++) {
-            console.log(itinArray[i].city);
-            console.log(itinArray[i].startDate);
-            console.log(itinArray[i].endDate);
-            console.log('-------------');
-        }
-        for (var i = 0; i < itinArray.length; i++) {
-            var itinListItem = $("<li></li>");
-            var itinA = $("<a></a>");
-
-            itinList.addClass("ui-menu-item");
-            itinList.attr("role", "menuitem");
-
-            itinA.addClass("ui-all");
-            itinA.attr("tabindex", "-1");
-
-            itinA.text(itinArray[i].city);
-            itinListItem.append(itinA);
-
-            itinList.append(itinListItem);
-        }
-        $("#").append(itinList);
-    });
-});
 
 function contactHtmlFromObject(contact){
     console.log( contact );
